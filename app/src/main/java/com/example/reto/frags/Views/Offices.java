6591e28fae5316b6a9e87d76b8c166f22e63fd07
Ru-kko <verase2003.1@gmail.com>
2021-12-11 22:13:19 -0500

    All resources from fourth challenge

--- app/src/main/java/com/example/reto/frags/Views/Offices.java
+++ app/src/main/java/com/example/reto/frags/Views/Offices.java
@@ -1,12 +1,15 @@
 package com.example.reto.frags.Views;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.fragment.app.Fragment;
 
 import com.example.reto.BuildConfig;
@@ -16,17 +19,14 @@ import org.osmdroid.config.Configuration;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapController;
 import org.osmdroid.views.MapView;
-import org.osmdroid.views.overlay.ItemizedIconOverlay;
-import org.osmdroid.views.overlay.ItemizedOverlayWithFocus;
-import org.osmdroid.views.overlay.OverlayItem;
-import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
-import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.infowindow.InfoWindow;
 
-import java.util.ArrayList;
 
 public class Offices extends Fragment {
     MapView map;
     MapController mapController;
+    ViewGroup container;
 
     public Offices(){
         super(R.layout.offices_page);
@@ -35,6 +35,7 @@ public class Offices extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        this.container = container;
         return super.onCreateView(inflater, container, savedInstanceState);
     }
 
@@ -49,37 +50,55 @@ public class Offices extends Fragment {
         map.getZoomController().setZoomInEnabled(true);
         map.getZoomController().setZoomOutEnabled(true);
         map.setMultiTouchControls(true);
-        mapController.setZoom(16f);
-
-        ArrayList<OverlayItem> points = new ArrayList<>();
-
-        points.add(new OverlayItem("Office 1st Mayo", "", new GeoPoint(4.601050, -74.122368)));
-        points.add(new OverlayItem("Office Chapinero", "", new GeoPoint(4.650567, -74.063160)));
+        mapController.setZoom(14f);
 
 
-        final MyLocationNewOverlay myLocation = new MyLocationNewOverlay(new GpsMyLocationProvider(requireContext()), map);
-        map.getOverlays().add(myLocation);
-        myLocation.enableMyLocation();
-
-        ItemizedIconOverlay.OnItemGestureListener<OverlayItem> tap = new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
-            @Override
-            public boolean onItemLongPress(int arg0, OverlayItem arg1) {
-                return false;
-            }
-            @Override
-            public boolean onItemSingleTapUp(int index, OverlayItem item) {
-                return true;
-            }
+        Inf_window[] cap_ = {
+                new Inf_window("1st Mayo", new GeoPoint(4.601050, -74.122368)),
+                new Inf_window("Chapinero", new GeoPoint(4.650567, -74.063160))
         };
 
+        for (Inf_window i: cap_) {
+            Marker marker = new Marker(map, requireContext());
+            Drawable ico = AppCompatResources.getDrawable(requireContext() ,R.drawable.pin_ico);
 
-        ItemizedOverlayWithFocus<OverlayItem> cap = new ItemizedOverlayWithFocus<>(requireContext(), points, tap);
 
-        cap.setFocusItemsOnTap(true);
-        map.getOverlays().add(cap);
+            assert ico != null;
+            ico.setTint(getResources().getColor(R.color.purple_contrast));
+            marker.setPosition(i.point);
+            marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+            marker.setInfoWindow(i);
+
+            marker.setIcon(ico);
+            map.getOverlays().add(marker);
+        }
 
         GeoPoint Bogota = new GeoPoint(4.6482837,-74.2478938);
         mapController.setCenter(Bogota);
     }
 
+    private class Inf_window extends InfoWindow{
+        public GeoPoint point;
+        private final String name;
+
+        public Inf_window(String name, GeoPoint point){
+            super(R.layout.map_info_win, map);
+            this.point = point;
+            this.name = name;
+        }
+
+        @Override
+        public void onOpen(Object item) {
+            View view = getView();
+            TextView nameView = view.findViewById(R.id.map_inf_txt);
+            nameView.setText(name);
+            closeAllInfoWindowsOn(map);
+
+        }
+
+        @Override
+        public void onClose() {
+
+        }
+    }
 }
